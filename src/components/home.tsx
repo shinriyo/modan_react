f793262563d9c8d807bf4be25bd10fcdc29f8941
shinriyo <shinriyo@gmail.com>
2019-02-12 03:39:08 +0900

    containers components

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,11 +5,17 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
+import Grow from '@material-ui/core/Grow'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+
+import { HomeActions } from '../containers/home'
+import { AppState } from '../store'
 
 import { hocFactory } from '../helpers/hocHello'
 
 interface OwnProps {}
-
 const styles = (theme: Theme) => {
     return createStyles({
         layout: {
@@ -21,10 +27,15 @@ const styles = (theme: Theme) => {
                 marginRight: 'auto',
             },
         },
+        card: {
+            width: 275,
+            height: 300,
+        }
   })
 }
 
-type Props = OwnProps & WithStyles<typeof styles>
+// type Props = OwnProps & WithStyles<typeof styles>
+type Props = OwnProps & AppState & HomeActions & WithStyles<typeof styles>
 
 const HomeComponent: React.SFC<Props> = (props: Props) => {
   const { classes } = props
@@ -38,11 +49,22 @@ const HomeComponent: React.SFC<Props> = (props: Props) => {
         </AppBar>
       <div className={classes.layout}>
         <Typography>this is home component</Typography>
-        <Button>Open</Button>
+        <Button onClick={props.handleOpen}>Open</Button> 
+
+        <Grow in={props.card.open}>
+            <Card className={classes.card}>
+                <CardContent>
+                    <Typography>this is card.</Typography>
+                </CardContent>
+                <CardActions>
+                    <Button onClick={props.handleClose}>Close</Button>
+                </CardActions>
+            </Card>
+        </Grow>
       </div>
     </div>
   )
 }
 
 // export default withStyles(styles)(HomeComponent) 
-export default withStyles(styles)(HomeComponent) 
+export default withStyles(styles)(hocFactory(HomeComponent))
